86ae37bdad04fe6b286b3e60b148bf770d981233
Berkekacar <kacarberke@icloud.com>
2024-08-25 18:02:31 +0300

    initialize the app

---
+++ TicTocGame/TicTocGame/TicTocGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTocGameApp.swift
+//  TicTocGame
+//
+//  Created by Berke Kaçar on 25.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct TicTocGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
