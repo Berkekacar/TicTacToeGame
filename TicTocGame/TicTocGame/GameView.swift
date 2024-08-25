974b1dae967f62674ab9dd0ecac94767235a49f4
Berkekacar <kacarberke@icloud.com>
2024-08-25 19:10:27 +0300

    game view

--- TicTocGame/TicTocGame/GameView.swift
+++ TicTocGame/TicTocGame/GameView.swift
@@ -8,9 +8,50 @@
 import SwiftUI
 
 struct GameView: View {
+    
+    @StateObject var gameState = GameState()
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 50){
+            HStack{
+                Text("Turn \(gameState.turn)")
+                    .font(.system(size: 30))
+                    .bold()
+            }
+            VStack(spacing: 5){
+                ForEach(0...2, id: \.self){
+                    row in
+                    HStack(spacing: 5){
+                        ForEach(0...2, id: \.self){
+                            column in
+                            
+                            let cell = gameState.board[row][column]
+                            
+                            Text(cell.displayTile())
+                                .font(.system(size: 60))
+                                .foregroundStyle(cell.tileColor())
+                                .bold()
+                                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                                .aspectRatio(1,contentMode: .fit)
+                                .background(Color.white)
+                                .onTapGesture {
+                                    gameState.placeTile(row: row, column: column)
+                                }
+                        }
+                    }
+                }
+            }.background(Color.black)
+                .padding()
+                .alert(isPresented: $gameState.showAlert){
+                    Alert(
+                        title: Text("Oyun Bitti"),
+                        dismissButton: .default(Text("Okey")){
+                            gameState.resetBoard()
+                        }
+                    )
+                }
+        }
     }
+    
 }
 
 #Preview {
